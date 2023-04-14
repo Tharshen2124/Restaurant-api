14089eca949b1ef1e446c4e54f9f44b5df33ad86
Tharshen2124 <tharshensurianbalan@gmail.com>
2023-04-14 20:21:25 +0800

    initialised repo

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Menu>
+ */
+class MenuFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'menu_item' => $this->faker->name,
+            'price' => $this->faker-> randomFloat(1, 10, 20),
+        ];
+    }
+}
