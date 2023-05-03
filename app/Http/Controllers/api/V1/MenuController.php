430cb700640b07de3ec68e14bc96616d4cfa7545
Tharshen2124 <tharshensurianbalan@gmail.com>
2023-05-03 10:32:18 +0800

    clean up MenuController

--- app/Http/Controllers/api/V1/MenuController.php
+++ app/Http/Controllers/api/V1/MenuController.php
@@ -13,56 +13,31 @@ use App\Http\Requests\V1\UpdateMenuRequest;
 
 class MenuController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(Menu $menu)
-    {
+    //Display a listing of the resource.
+    public function index() {
         return new MenuCollection(Menu::paginate());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreMenuRequest $request)
-    {
+    //Store a newly created resource in storage.
+    public function store(StoreMenuRequest $request) {
         return new MenuResource(Menu::create($request->all()));
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Menu $menu)
-    {
+    //Display the specified resource.
+    public function show(Menu $menu){
         return new MenuResource($menu);
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateMenuRequest $request, string $id)
-    {
-        $menu = Menu::find($id);
-
+    
+    //Update the specified resource in storage.
+    public function update(UpdateMenuRequest $request, Menu $menu) {
         $menu->update($request->all());
     }
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Menu $menu)
-    {
+    
+    //Remove the specified resource from storage.
+    public function destroy(Menu $menu) {
         $id = $menu->id;
-
         $menu->delete();
 
-        if(Menu::find($id) === null) {
-            return [
-                "message" => "menu item has been deleted"
-            ];
-        } else {
-            return [
-                "message" => "looks like the item is still here"            
-            ];
-        }
+        return Menu::find($id) === null ? ["message" => "menu item deleted successfully!"] : ["message" => "menu item deletion was unsuccessful"];
     }
 }
