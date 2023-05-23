9199dfa0aea9f8117ed84bcea8dc9de33fe86d8b
Tharshen2124 <tharshensurianbalan@gmail.com>
2023-05-23 18:58:32 +0800

    Revert "fix:modify the api routes and edit the OrderitemController"
    This reverts commit e02e7f86f89a12edc15c33493af059154d7619f8.

--- app/Http/Controllers/api/V1/OrderitemController.php
+++ app/Http/Controllers/api/V1/OrderitemController.php
@@ -13,16 +13,24 @@ use App\Http\Requests\V1\UpdateOrderitemRequest;
 
 class OrderitemController extends Controller
 {
-    // Display a listing of the resource.
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-    
+        //
     }
 
-    // Store a newly created resource in storage.
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(StoreOrderitemRequest $request, Menu $menu)
     {
-        /* $attributes = $request->validate($request->all); */
+        $numOfItems = Cache::get('numOfItems');
+        
+        $attributes = $request->validate([
+            'quantity' => 'integer|min:1',
+        ]);
         
         // /https://laravel.com/docs/10.x/eloquent#retrieving-or-creating-models
         // use firstorcreate to create order if (status = pending & order with user id doesnt exist)
@@ -33,31 +41,40 @@ class OrderitemController extends Controller
             ],
             [ "payment" => 0 ]
         );
+        
+        $orderId = $order->id;
+
+        $numOfItems += $attributes['quantity'];
+        Cache::put('numOfItems', $numOfItems);
 
         $orderitem = new Orderitem;
         $orderitem->order_id = $order->id;
         $orderitem->menu_id = $menu->id;
-        $orderitem->quantity = $request->quantity;
-        $isAdded = $orderitem->save();
-
-        return $isAdded ? ['message' => 'Item added to cart'] : ['message' => 'an error occured'];
+        $orderitem->quantity = $attributes['quantity'];
+        $orderitem->save();
     }
 
-    // Display the specified resource.
+    /**
+     * Display the specified resource.
+     */
     public function show(Orderitem $orderitem)
     {
-        
+        //
     }
 
-    // Update the specified resource in storage.
+    /**
+     * Update the specified resource in storage.
+     */
     public function update(UpdateOrderitemRequest $request, Orderitem $orderitem)
     {
-        
+        //
     }
 
-    // Remove the specified resource from storage.
+    /**
+     * Remove the specified resource from storage.
+     */
     public function destroy(Orderitem $orderitem)
     {
-        
+        //
     }
 }
